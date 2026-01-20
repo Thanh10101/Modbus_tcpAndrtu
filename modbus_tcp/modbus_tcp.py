b12d58dd5ee35af282f97b6d32e3bb099ac2de78
Nguyen Trinh Thanh <thanhnt@tamatra.local>
2026-01-20 09:33:54 +0700

    v1-check-connection

--- modbus_tcp/modbus_tcp.py
+++ modbus_tcp/modbus_tcp.py
@@ -1,21 +1,25 @@
 from pymodbus.client import ModbusTcpClient
 
-client = ModbusTcpClient("172.16.22.87", port=502)
+IP = "172.16.22.87"
+PORT = 502
 
-if client.connect():
-    print("Kết nối Modbus TCP thành công")
 
-    response = client.read_holding_registers(
-    address=0,
-    count=2
+client = ModbusTcpClient(IP, port=PORT)
+client.connect()
+
+valid_addresses = []
+
+for addr in range(0, 200):  # quét 0 → 199
+    result = client.read_holding_registers(
+        address=addr,
+        count=1,
+    
     )
 
+    if not result.isError():
+        print(f"✅ Address {addr} có giá trị: {result.registers[0]}")
+        valid_addresses.append(addr)
 
-    if not response.isError():
-        print("Giá trị:", response.registers)
-    else:
-        print("Lỗi đọc Modbus")
+client.close()
 
-    client.close()
-else:
-    print("Không kết nối được PLC")
+print("Address hợp lệ:", valid_addresses)
